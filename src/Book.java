cacfdaea14564d66af1766aa02ed76b23d6c33b1
ABDULL-44 <mohamedbasith965@gmail.com>
2024-05-27 11:19:25 +0530

    my 2 commit

---
+++ src/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+
+    public static void main(String[] args) {
+
+        System.out.println("Book 2");
+        System.out.println("Zootopiya");
+        System.out.println("234 pages");
+    }
+}
---
+++ src/Library_System.java
@@ -0,0 +1,7 @@
+public class Library_System {
+    public static void main(String[] args) {
+
+        System.out.println("Abdull");
+        System.out.println(18);
+    }
+}
